fc281b99c9e7bf5e641a392097c9875bef4cf809
Vitthal Gadekar <gadekarvitthal2@gmail.com>
2025-07-06 15:07:10 +0530

    sample

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/User');
 require('dotenv').config();
 
 // Register route
