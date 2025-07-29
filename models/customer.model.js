831fe4a88b84ae1fc32f51eb97e95aaf76df9fbc
Vitthal Gadekar <gadekarvitthal2@gmail.com>
2025-07-29 10:52:02 +0530

    bug fix new register not adding

--- models/customer.model.js
+++ models/customer.model.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const customerSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   // email:    { type: String, required: false ,sparse: true},
-  phone:    { type: String, required: true, unique: true },
-  address:  { type: String, required: true },
+  phone:    { type: String, required: true, unique: false },
+  address:  { type: String, required: false },
   bookingDate: Date,         // <-- Add this
   totalAmount: Number  , // <-- Add this
   isDelivered: Boolean  , // <-- Add this
   pickupPreference: { type: String, enum: ['Home', 'Shop'], default: 'Shop' },
   createdAt: { type: Date, default: Date.now },
-  billNumber:{ type: String, required: false,unique: true}
+  billNumber: { type: String, default: null }, // Removed unique constraint and field, not needed for customers
 });
 
 module.exports = mongoose.model('Customer', customerSchema);
